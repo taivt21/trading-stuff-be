fc50fe6df60990221e06ab11b072bf3e32f1e769
Vinh Tran <53442440+tranvinh99@users.noreply.github.com>
2023-07-11 14:15:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 import app from "./app.js";
 
+import mongoose from 'mongoose'
+
 const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.set('strictQuery', true)
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB database");
+});
 
 app.listen(port, async () => {
   // await connect();
