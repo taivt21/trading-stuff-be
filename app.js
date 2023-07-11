70ff8999bce8abb37747515c636845957c94e9f8
Vinh Tran <53442440+tranvinh99@users.noreply.github.com>
2023-07-11 14:05:11 +0700

    check connect db

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ import "dotenv/config";
 import cors from "cors";
 import bodyParser from "body-parser";
 import routes from "./routes/index.js";
+import mongoose from "mongoose";
 
 const app = express();
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("connect db successfully");
+});
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
