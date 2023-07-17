dfed2c6d1dc0064e29257ef1814ea07151c4fbd1
Vinh Tran <53442440+tranvinh99@users.noreply.github.com>
2023-07-17 21:23:16 +0700

    add user controller function

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,13 +1,22 @@
 import User from "../entities/user.js";
 
 export const getUsers = async (req, res) => {
-  const users = await User.find({});
+  const query = req.query;
+
+  const totalUser = await User.count();
+
+  const limit = query.limit ?? {};
+
+  const page = query.page ?? {};
+
+  const skip = totalUser - limit * page;
+
+  const users = await User.find({}).limit(limit).skip(skip);
+
   res.status(200).json(users);
 };
 
 export const getProfile = async (req, res) => {
-  const { id } = req.user;
-
   const user = await User.findById(id);
 
   res.status(200).json(user);
@@ -23,4 +32,24 @@ export const editUser = async (req, res) => {
   res.status(200).json({
     user: await User.findById(id),
   });
+};
+
+export const getUserInWithinDay = async (req, res) => {
+  const { gte } = req.query;
+
+  console.log("file: user.controller.js:28 ~ getUserInMonth ~ day:", gte);
+
+  const thirtyDaysAgo = new Date();
+
+  thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - gte);
+
+  const users = await User.aggregate([
+    {
+      $match: {
+        createdAt: { $gte: thirtyDaysAgo },
+      },
+    },
+  ]);
+
+  res.status(200).json({ users });
 };
